ca1792f3d6410c501fa517fc31587f089ff0f2bb
cacoinchik <mr.ruslan126@mail.ru>
2025-10-13 00:38:13 +0300

    Добавьте файлы проекта.

---
+++ src/WebApps/Shop.Web/Shop.Web/Program.cs
@@ -0,0 +1,33 @@
+using Shop.Web.Client.Pages;
+using Shop.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Shop.Web.Client._Imports).Assembly);
+
+app.Run();
