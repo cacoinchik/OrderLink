ea25174ce1eed8cd9e66d497224fd72f3c392fec
cacoinchik <mr.ruslan126@mail.ru>
2025-11-09 16:15:33 +0300

    Inventory: добавлено логирование для Kafka Orders: добавлено логирование для Kafka

--- src/Services/Inventory/Inventory.API/Kafka/Consumers/OrderEventConsumer.cs
+++ src/Services/Inventory/Inventory.API/Kafka/Consumers/OrderEventConsumer.cs
@@ -7,14 +7,26 @@ namespace Inventory.API.Kafka.Consumers
 {
     public class OrderEventConsumer : BackgroundService
     {
-        private readonly IConsumer<string, string> _consumer;
+        private IConsumer<string, string>? _consumer;
         private readonly KafkaSettings _kafkaSettings;
         private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<OrderEventConsumer> _logger;
 
-        public OrderEventConsumer(IOptions<KafkaSettings> kafkaSettings, IServiceProvider serviceProvider)
+        public OrderEventConsumer(IOptions<KafkaSettings> kafkaSettings, IServiceProvider serviceProvider, ILogger<OrderEventConsumer> logger)
         {
             _kafkaSettings = kafkaSettings.Value;
             _serviceProvider = serviceProvider;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Yield();
+
+            _logger.LogInformation("Starting Kafka Consumer...");
+            _logger.LogInformation($"Kafka BootstrapServers: {_kafkaSettings.BootstrapServers}");
+            _logger.LogInformation($"Kafka GroupId {_kafkaSettings.GroupId}");
+            _logger.LogInformation($"Kafka Topics {_kafkaSettings.Topics.InventoryEvents}");
 
             var config = new ConsumerConfig
             {
@@ -24,12 +36,13 @@ namespace Inventory.API.Kafka.Consumers
                 EnableAutoCommit = false
             };
 
+            _logger.LogInformation("Kafka creating consumer");
             _consumer = new ConsumerBuilder<string, string>(config: config).Build();
-        }
+            _logger.LogInformation("Consumer created");
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
+            _logger.LogInformation($"Subscribing to topic: {_kafkaSettings.Topics.OrderEvents}");
             _consumer.Subscribe(_kafkaSettings.Topics.OrderEvents);
+            _logger.LogInformation("Successfully subscribed to topic");
 
             try
             {
@@ -41,6 +54,7 @@ namespace Inventory.API.Kafka.Consumers
 
                         if (consumeResult is not null)
                         {
+                            _logger.LogInformation($"Message recieved from Kafka: {consumeResult.Message.Key}");
                             await HandleMessageAsync(consumeResult, stoppingToken);
                             _consumer.Commit(consumeResult);
                         }
@@ -63,6 +77,10 @@ namespace Inventory.API.Kafka.Consumers
 
         private async Task HandleMessageAsync(ConsumeResult<string, string> result, CancellationToken cancellationToken)
         {
+            _logger.LogInformation($"Message received from Kafka - Key: {result.Message.Key}, Value: {result.Message.Value}");
+
+            //TODO Обработка сообщения
+
             await Task.CompletedTask;
         }
     }
--- src/Services/Orders/Orders.API/Kafka/Consumers/InventoryEventConsumer.cs
+++ src/Services/Orders/Orders.API/Kafka/Consumers/InventoryEventConsumer.cs
@@ -2,55 +2,85 @@
 using Confluent.Kafka.Admin;
 using Microsoft.Extensions.Options;
 using Orders.API.Kafka.Configuration;
+using System.Security.Cryptography;
 
 namespace Orders.API.Kafka.Consumers
 {
     public class InventoryEventConsumer : BackgroundService
     {
-        private readonly IConsumer<string, string> _consumer;
+        private IConsumer<string, string>? _consumer;
         private readonly KafkaSettings _kafkaSettings;
         private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<InventoryEventConsumer> _logger;
 
-        public InventoryEventConsumer(IOptions<KafkaSettings> kafkaSettings, IServiceProvider serviceProvider)
+
+        public InventoryEventConsumer(IOptions<KafkaSettings> kafkaSettings, IServiceProvider serviceProvider, ILogger<InventoryEventConsumer> logger)
         {
             _kafkaSettings = kafkaSettings.Value;
             _serviceProvider = serviceProvider;
-
-            var config = new ConsumerConfig
-            {
-                BootstrapServers = _kafkaSettings.BootstrapServers,
-                GroupId = _kafkaSettings.GroupId,
-                AutoOffsetReset = AutoOffsetReset.Earliest,
-                EnableAutoCommit = false
-            };
-
-            _consumer = new ConsumerBuilder<string, string>(config: config).Build();
+            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _consumer.Subscribe(_kafkaSettings.Topics.InventoryEvents);
+            await Task.Yield();
+
+            _logger.LogInformation("Starting Kafka Consumer...");
+            _logger.LogInformation($"Kafka BootstrapServers: {_kafkaSettings.BootstrapServers}");
+            _logger.LogInformation($"Kafka GroupId: {_kafkaSettings.GroupId}");
+            _logger.LogInformation($"Kafka Topics: {_kafkaSettings.Topics.InventoryEvents}");
 
             try
             {
+                var config = new ConsumerConfig
+                {
+                    BootstrapServers = _kafkaSettings.BootstrapServers,
+                    GroupId = _kafkaSettings.GroupId,
+                    AutoOffsetReset = AutoOffsetReset.Earliest,
+                    EnableAutoCommit = false
+                };
+
+                _logger.LogInformation("Kafka creating consumer");
+                _consumer = new ConsumerBuilder<string, string>(config: config).Build();
+                _logger.LogInformation("Consumer created");
+
+                _logger.LogInformation($"Subscribing to topic: {_kafkaSettings.Topics.InventoryEvents}");
+                _consumer.Subscribe(_kafkaSettings.Topics.InventoryEvents);
+                _logger.LogInformation("Successfully subscribed to topic");
+
                 while (!stoppingToken.IsCancellationRequested)
                 {
                     try
                     {
                         var consumeResult = _consumer.Consume(TimeSpan.FromSeconds(1));
 
-                        if(consumeResult is not null)
+                        if (consumeResult is not null)
                         {
+                            _logger.LogInformation($"Message recieved from Kafka: {consumeResult.Message.Key}");
                             await HandleMessageAsync(consumeResult, stoppingToken);
                             _consumer.Commit(consumeResult);
                         }
                     }
-                    catch (Exception ex) { }
+                    catch (ConsumeException ex)
+                    {
+                        _logger.LogError(ex, "Error consuming message from Kafka");
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "Unexpected error");
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, "Fatal Error - service will stop");
+                throw;
             }
             finally
             {
-                _consumer.Close();
+                _logger.LogInformation("CLosing Kafka consumer");
+                _consumer?.Close();
+                _logger.LogInformation("Kafka consumer closed");
             }
         }
 
@@ -62,6 +92,10 @@ namespace Orders.API.Kafka.Consumers
 
         private async Task HandleMessageAsync(ConsumeResult<string, string> result, CancellationToken cancellationToken)
         {
+            _logger.LogInformation($"Message received from Kafka - Key: {result.Message.Key}, Value: {result.Message.Value}");
+
+            //TODO Обработка сообщения
+
             await Task.CompletedTask;
         }
     }
